264ae32dad936d16b477a94ad01b91584b2c7d9a
polamjag <s@polamjag.info>
2015-08-08 12:23:56 +0900

    refactor and add REST API

--- main.rb
+++ main.rb
@@ -11,94 +11,188 @@ require 'redis'
 
 require 'slim'
 
-configure do
-  set :server, 'thin'
-  set :sockets, []
-  set :ws_url, ENV["WS_URL"]
+class Subscriber
+  attr_reader :subscribers
+
+  def initialize
+    @subscribers = []
+  end
+
+  def add ws
+    @subscribers << ws
+  end
+
+  def delete ws
+    @subscribers.delete ws
+  end
+
+  def send ws, message
+    ws.send message
+  end
+
+  def broadcast message
+    @subscribers.each do |sub|
+      sub.send message
+    end
+  end
+
+  def count
+    @subscribers.size
+  end
 end
 
-tracks = [
-  {
-    slug: "エモい",
-    path: "/mp3/emoi.mp3",
-    description: ""
-  },
-  {
-    slug: "最高",
-    path: "/mp3/saiko.mp3",
-    description: ""
-  },
-  {
-    slug: "銅鑼",
-    path: "/mp3/Gong-266566.mp3",
-    description: ""
-  },
-  {
-    slug: "Frontliner",
-    path: "/mp3/frontliner-fsharp-kick.mp3",
-    description: ""
-  }
-]
+class Sampler
+  def initialize tracks, redis
+    @tracks = tracks
+    @redis = redis
+  end
 
-redis = Redis.new host: "127.0.0.1", port:"6379"
+  def has_track? slug
+    @tracks.select do |track|
+      track[:slug] == slug
+    end.size == 1
+  end
 
-get '/api/tracks' do
-  tracks.each do |track|
-    track[:count] = redis.get(track[:slug])
-  end.to_json
+  def get_count slug
+    @redis.get slug if has_track? slug
+  end
+
+  def incr slug
+    @redis.incr slug if has_track? slug
+  end
+
+  def tracks
+    @tracks.map do |track|
+      track[:count] = @redis.get track[:slug]
+      track
+    end
+  end
+
+  def track_info slug
+    t = @tracks.select do |track|
+      track[:slug] == slug
+    end.first
+    t[:count] = @redis.get slug
+    t
+  end
+end
+
+class InternetSampler
+  def initialize subscribers, sampler
+    @sub = subscribers
+    @sampler = sampler
+  end
+
+  def play slug
+    if @sampler.has_track? slug
+      i = @sampler.incr slug
+      EM.next_tick do
+        @sub.broadcast({
+          type: :play,
+          count: i,
+          slug: slug
+        }.to_json)
+      end
+    else
+      puts :err
+    end
+  end
+
+  def add ws
+    @sub.add ws
+  end
+
+  def delete ws
+    @sub.delete ws
+  end
+
+  def update_subscribers_count
+    @sub.broadcast({
+      type: :num,
+      count: @sub.count,
+    }.to_json)
+  end
+
+  def tracks
+    @sampler.tracks
+  end
+
+  def track_info slug
+    @sampler.track_info slug
+  end
+end
+
+configure do
+  set :server, 'thin'
+  set :ws_url, ENV["WS_URL"]
+  set :is, InternetSampler.new(
+    Subscriber.new,
+    Sampler.new(
+      [
+        {
+          slug: "エモい",
+          path: "/mp3/emoi.mp3",
+          description: ""
+        },
+        {
+          slug: "最高",
+          path: "/mp3/saiko.mp3",
+          description: ""
+        },
+        {
+          slug: "銅鑼",
+          path: "/mp3/Gong-266566.mp3",
+          description: ""
+        },
+        {
+          slug: "Frontliner",
+          path: "/mp3/frontliner-fsharp-kick.mp3",
+          description: ""
+        }
+      ],
+      Redis.new(host: "127.0.0.1", port:"6379")
+    )
+  )
 end
 
 get '/' do
   if !request.websocket?
     @ws_url = (settings.ws_url || "ws://#{request.env['HTTP_HOST']}/")
-    @tracks = tracks.each do |track|
-      track[:count] = redis.get(track[:slug])
-    end
+    @tracks = settings.is.tracks
+    p @tracks
     slim :index
   else
     request.websocket do |ws|
-
       ws.onopen do
+        settings.is.add ws
         ws.send({
           type: :msg,
           msg: "WebSocket connected!"
         }.to_json)
-        settings.sockets << ws
-        settings.sockets.each do |s|
-          s.send({
-            type: :num,
-            count: settings.sockets.length
-          }.to_json)
-        end
+        settings.is.update_subscribers_count
       end
 
       ws.onmessage do |msg|
-        if tracks.any? { |t| t[:slug] == msg.to_s }
-          puts "Play: #{msg}"
-          count = redis.incr(msg.to_s)
-          EM.next_tick do
-            settings.sockets.each do |s|
-              s.send({
-                type: :play,
-                count: count,
-                slug: msg
-              }.to_json)
-            end
-          end
-        end
+        puts "Play: #{msg}"
+        settings.is.play msg
       end
 
       ws.onclose do
         warn("WebSocket closed")
-        settings.sockets.delete(ws)
-        settings.sockets.each do |s|
-          s.send({
-            type: :num,
-            count: settings.sockets.length
-          }.to_json)
-        end
+        settings.is.delete ws
+        settings.is.update_subscribers_count
       end
-
     end
   end
+end
+
+get '/api/v1/tracks' do
+  content_type 'application/json'
+  settings.is.tracks.to_json
+end
+
+get '/api/v1/tracks/:slug/play' do
+  content_type 'application/json'
+  settings.is.play params[:slug]
+  settings.is.track_info(params[:slug]).to_json
 end

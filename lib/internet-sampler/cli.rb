b8a2038a8caf8281c8b8a7b2a3c65d5daa7f4cac
polamjag <s@polamjag.info>
2015-12-14 05:21:50 +0900

    change default port to 9292

--- lib/internet-sampler/cli.rb
+++ lib/internet-sampler/cli.rb
@@ -6,7 +6,7 @@ module InternetSampler
   class CLI
     def initialize args
       sinatra_options = {
-        port: 8080,
+        port: 9292,
         bind: 'localhost',
       }
       sampler_options = {
@@ -15,7 +15,7 @@ module InternetSampler
 
       opt = OptionParser.new
 
-      opt.on('-p port', '--port port', 'port to listen (default is 8080)') { |i| sinatra_options[:port] = i.to_i }
+      opt.on('-p port', '--port port', 'port to listen (default is 9292)') { |i| sinatra_options[:port] = i.to_i }
       opt.on('-b host', '--bind host', 'host to bind (default is localhost)') { |i| sinatra_options[:bind] = i }
       opt.on('-t slug:url', '--track slug:url', 'sample sound to serve (note that `url\' is not path to file but URL to serve)') { |i|
         if track = i.match(/^([^:]*):(.*)$/)

22b001dd42c41cd245db331594623ac3564155bb
polamjag <s@polamjag.info>
2015-12-14 05:29:55 +0900

    fix spec path

---
+++ spec/internet-sampler_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe InternetSampler do
+  it 'has a version number' do
+    expect(InternetSampler::VERSION).not_to be nil
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,2 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'internet/sampler'
+require 'internet-sampler'

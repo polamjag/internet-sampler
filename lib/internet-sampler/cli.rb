213b1e919a656cdaf07bc7507361c33ecca9b3e2
polamjag <s@polamjag.info>
2015-12-14 12:35:00 +0900

    treat env

--- lib/internet-sampler/cli.rb
+++ lib/internet-sampler/cli.rb
@@ -8,6 +8,7 @@ module InternetSampler
       sinatra_options = {
         port: 9292,
         bind: 'localhost',
+        environment: :development,
       }
       sampler_options = {
         tracks: []
@@ -17,6 +18,11 @@ module InternetSampler
 
       opt.on('-p port', '--port port', 'port to listen (default is 9292)') { |i| sinatra_options[:port] = i.to_i }
       opt.on('-b host', '--bind host', 'host to bind (default is localhost)') { |i| sinatra_options[:bind] = i }
+      opt.on('-e env', '--environment env', 'environment to run (production, test or development; default is development)') { |i|
+        if %w{development test production}.include? i
+          sinatra_options[:environment] = i.to_sym
+        end
+      }
       opt.on('-t slug:url', '--track slug:url', 'sample sound to serve (note that `url\' is not path to file but URL to serve)') { |i|
         if track = i.match(/^([^:]*):(.*)$/)
           sampler_options[:tracks] << {
